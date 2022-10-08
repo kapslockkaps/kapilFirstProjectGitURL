2b00d6c71e1be00256366b954f709ee6ec3b437a
kapslockkaps <kapslock.kaps@outlook.com>
2022-10-08 10:48:42 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Startup
+    {      
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR()
+                    .AddAzureSignalR(Configuration["SIGNALR_CONNECTIONSTRING"]);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            /* Setup SignalR routes by calling endpoint.MapHub */
+            /* Maps incoming requests with the specified path to the specified Microsoft.AspNetCore.SignalR.Hub type. */
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatSampleHub>("/chat111");
+            });
+        }
+    }
+}
